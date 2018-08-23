b517dfc87282f256220b3a32e9344661c3aac493
Kara Cavanaugh <karacav@msn.com>
2018-08-22 20:11:08 -0400

    Create NewObject.java

---
+++ NewObject.java
@@ -0,0 +1,7 @@
+public class NewObject{
+  public static void main(String[] args){
+   System.out.println("start"); 
+   MyFirstObject myFirstObject = new MyFirstObject();
+   System.out.println(++myFirstObject.x);
+  }
+}
